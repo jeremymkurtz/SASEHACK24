99f73668b17b217e1d2e214c16646004d737871f
brandonyeu <brandonyeu8@gmail.com>
2024-10-04 19:41:20 -0400

    Commented out auth0 stuff for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
-import {ReflectAdapter as app} from "next/dist/server/web/spec-extension/adapters/reflect";
+// import {ReflectAdapter as app} from "next/dist/server/web/spec-extension/adapters/reflect";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
-
-const { auth } = require('express-openid-connect');
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'a long, randomly-generated string stored in env',
-    baseURL: 'http://localhost:3000',
-    clientID: 'ZC9BOffbGZl2Efn5htEFL0zRp5UiReFr',
-    issuerBaseURL: 'https://dev-jdflgo8as3oue166.us.auth0.com'
-};
+//
+// const { auth } = require('express-openid-connect');
+//
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: 'a long, randomly-generated string stored in env',
+//     baseURL: 'http://localhost:3000',
+//     clientID: 'ZC9BOffbGZl2Efn5htEFL0zRp5UiReFr',
+//     issuerBaseURL: 'https://dev-jdflgo8as3oue166.us.auth0.com'
+// };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
